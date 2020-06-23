02190c8d3da53cc42f9ec0da66dd9aa46f16f3ac
thdus3009 <thdus3009@gmail.com>
2020-06-23 10:06:29 +0900

    페이징 처리 가져오기

--- src/main/java/com/mega/s1/member/admin/AdminRepository.java
+++ src/main/java/com/mega/s1/member/admin/AdminRepository.java
@@ -1,8 +1,14 @@
 package com.mega.s1.member.admin;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import com.mega.s1.member.MemberVO;
 
 @Mapper
 public interface AdminRepository {
 
+	//memberList
+	public List<MemberVO> getMemberList() throws Exception;
 }
